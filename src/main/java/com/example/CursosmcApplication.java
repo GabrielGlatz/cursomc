1bd716ecc63e4664fd1bad3655bdd77450019fc4
Gabriel Glatz <gabrielglatz@hotmail.com>
2022-08-16 15:47:25 -0300

    Cliente, TipoCLiente, Telefones e Endereços

--- src/main/java/com/example/CursosmcApplication.java
+++ src/main/java/com/example/CursosmcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.domain.Categoria;
 import com.example.domain.Cidade;
+import com.example.domain.Cliente;
+import com.example.domain.Endereco;
 import com.example.domain.Estado;
 import com.example.domain.Produto;
+import com.example.domain.enums.TipoCliente;
 import com.example.repositories.CategoriaRepository;
 import com.example.repositories.CidadeRepository;
+import com.example.repositories.ClienteRepository;
+import com.example.repositories.EnderecoRepository;
 import com.example.repositories.EstadoRepository;
 import com.example.repositories.ProdutoRepository;
 
@@ -30,6 +35,13 @@ public class CursosmcApplication implements CommandLineRunner {
 	
 	@Autowired
 	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosmcApplication.class, args);
@@ -52,6 +64,10 @@ public class CursosmcApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva ", "maria@GMAIL.COM", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
 		/*AQUI EU ADICIONEI OS PRODUTOS NAS CATEGORIAS CORRESPONDENTES, EX: NA INFORMATICA TEM COMPUTADOR, IMPRESSORA E MOUSE*/
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
@@ -64,11 +80,17 @@ public class CursosmcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
 		/*AQUI TA SALVANDO AS LISTAS */
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
 	}
 	
 	
