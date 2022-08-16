579ebd4e2133b75e49e18ffadf29f5fe15883ab3
Gabriel Glatz <gabrielglatz@hotmail.com>
2022-08-16 11:07:27 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/example/CursosmcApplication.java
+++ src/main/java/com/example/CursosmcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.domain.Categoria;
+import com.example.domain.Produto;
 import com.example.repositories.CategoriaRepository;
+import com.example.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursosmcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriarepository;
+	
+	@Autowired
+	private ProdutoRepository produtorepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosmcApplication.class, args);
@@ -26,7 +31,23 @@ public class CursosmcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "informática");
 		Categoria cat2 = new Categoria(null, "escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora ", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);	
+		
+		
+		/*AQUI EU ADICIONEI OS PRODUTOS NAS CATEGORIAS CORRESPONDENTES, EX: NA INFORMATICA TEM COMPUTADOR, IMPRESSORA E MOUSE*/
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		/*AQUI TA SALVANDO A LISTA DE CATEGORIA */
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		produtorepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 	
