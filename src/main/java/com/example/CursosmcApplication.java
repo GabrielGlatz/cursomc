158d2d1de4287bad2a9c4f061c9745ade7fc3097
Gabriel Glatz <gabrielglatz@hotmail.com>
2022-08-16 09:55:20 -0300

    fazendo insert automatico no banco de dados

--- src/main/java/com/example/CursosmcApplication.java
+++ src/main/java/com/example/CursosmcApplication.java
@@ -1,13 +1,34 @@
 package com.example;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursosmcApplication {
+import com.example.domain.Categoria;
+import com.example.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursosmcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosmcApplication.class, args);
 	}
+     /* salvar as categorias no banco de dados*/
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "escritório");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 
 }
