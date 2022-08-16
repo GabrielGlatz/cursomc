e27e11818693d9696c4f21dc923683120615325b
Gabriel Glatz <gabrielglatz@hotmail.com>
2022-08-16 20:08:45 -0300

    Pedido, pagamento e estado pagamento

--- src/main/java/com/example/CursosmcApplication.java
+++ src/main/java/com/example/CursosmcApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.example.domain.Cidade;
 import com.example.domain.Cliente;
 import com.example.domain.Endereco;
 import com.example.domain.Estado;
+import com.example.domain.Pagamento;
+import com.example.domain.PagamentoComBoleto;
+import com.example.domain.PagamentoComCartao;
+import com.example.domain.Pedido;
 import com.example.domain.Produto;
+import com.example.domain.enums.EstadoPagamento;
 import com.example.domain.enums.TipoCliente;
 import com.example.repositories.CategoriaRepository;
 import com.example.repositories.CidadeRepository;
 import com.example.repositories.ClienteRepository;
 import com.example.repositories.EnderecoRepository;
 import com.example.repositories.EstadoRepository;
+import com.example.repositories.PagamentoRepository;
+import com.example.repositories.PedidoRepository;
 import com.example.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -26,73 +34,91 @@ public class CursosmcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	PedidoRepository pedidorepository;
+
+	@Autowired
+	PagamentoRepository pagamentorepository;
 	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosmcApplication.class, args);
 	}
-     /* salvar as categorias no banco de dados*/
-	
+	/* salvar as categorias no banco de dados */
+
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "informática");
 		Categoria cat2 = new Categoria(null, "escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora ", 800.00);
-		Produto p3 = new Produto(null, "mouse", 80.00);	
-		
+		Produto p3 = new Produto(null, "mouse", 80.00);
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva ", "maria@GMAIL.COM", "36378912377", TipoCliente.PESSOAFISICA);
-		
+
 		Endereco e1 = new Endereco(null, "Rua flores", "300", "apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
 		
-		/*AQUI EU ADICIONEI OS PRODUTOS NAS CATEGORIAS CORRESPONDENTES, EX: NA INFORMATICA TEM COMPUTADOR, IMPRESSORA E MOUSE*/
+		/*
+		 * AQUI EU ADICIONEI OS PRODUTOS NAS CATEGORIAS CORRESPONDENTES, EX: NA
+		 * INFORMATICA TEM COMPUTADOR, IMPRESSORA E MOUSE
+		 */
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
-		/*AQUI TA SALVANDO AS LISTAS */
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		/* AQUI TA SALVANDO AS LISTAS */
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-
+		pedidorepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentorepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
-	
-	
 
 }
--- src/main/java/com/example/domain/Cliente.java
+++ src/main/java/com/example/domain/Cliente.java
@@ -40,6 +40,9 @@ public class Cliente implements Serializable {
 	/*SET é um conjunto de strings que nao pode ter repetição*/
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -93,19 +96,7 @@ public class Cliente implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		return Objects.equals(id, other.id);
-	}
-
+	
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
@@ -125,6 +116,27 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
 	
 	
 }
